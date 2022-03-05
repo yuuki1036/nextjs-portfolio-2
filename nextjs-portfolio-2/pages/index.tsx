6ae412cf838c4f0fff488b39496c5bb87a440e88
yuuki1036 <yuuki1036@gmail.com>
2022-03-05 18:30:38 +0900

    fix eslint

--- nextjs-portfolio-2/pages/index.tsx
+++ nextjs-portfolio-2/pages/index.tsx
@@ -1,21 +1,8 @@
-import { css } from "@emotion/react";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import { styled } from "@mui/system";
+import React from "react";
 import MainVisual from "components/MainVisual";
-import MyAvatar from "components/MyAvatar";
 import Profile from "components/Profile";
 import Wrapper from "components/Wrapper";
 import type { NextPage } from "next";
-import Image from "next/image";
-import * as React from "react";
-import {
-  Parallax,
-  ParallaxBanner,
-} from "react-scroll-parallax";
-import Link from "../components/Link";
-import tateyama from "../public/images/tateyama.webp";
 
 const Home: NextPage = () => {
   return (
